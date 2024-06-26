9969b105008fbc7ad18b725b1e4db8deabb6e5e8
sanjay <sanjaykuttyags@gmail.com>
2024-06-26 21:38:34 +0530

    new Commit

--- palindrome/palindrome4.js
+++ palindrome/palindrome4.js
@@ -1,29 +1,20 @@
 let input = "malayalam";
 
 function Palindrome(str) {
+  let Reverse = "";
 
-    let Reverse = ''
-    
-   for(let i=str.length-1; i>=0; i--) {
- 
-     Reverse += str[i]
-   }
-    // Check if the reversed string is equal to the original string
-    if (str === Reverse) {
-        return true;
-    } else {
-        return false;
-    }
-
-
-
-
-
-  
+  for (let i = str.length - 1; i >= 0; i--) {
+    Reverse += str[i];
+  }
+  // Check if the reversed string is equal to the original string
+  if (str === Reverse) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-
-console.log(Palindrome(input));  // Output: true
+console.log(Palindrome(input)); // Output: true
 
 input = "madurai";
-console.log(Palindrome(input));  // Output: false
+console.log(Palindrome(input)); // Output: false
