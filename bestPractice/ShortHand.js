50d22ba29c0900acc8aaa2480c68e7a712936641
sanjay <sanjaykuttyags@gmail.com>
2024-07-31 09:43:26 +0530

    lasT

---
+++ bestPractice/ShortHand.js
@@ -0,0 +1,22 @@
+
+
+
+//Date and Time
+const currentDateTime = new Date()
+
+// console.log(currentDateTime)
+
+
+
+//Find max number in the array
+
+const numbers = [5,2,3,4,5]
+
+const max = Math.max(...numbers)
+const min = Math.min(...numbers)
+
+
+// console.log("maxnumber :" + max , "minnumber :" +  min)
+
+
+
